b87920966426d35706ebe5711778c9a2b63e842a
David Nimon <dnimon@mentortek.com>
2026-01-01 21:50:44 -0600

    restore patents page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ResumeComponent } from './pages/resume/resume.component';
 import { TechComponent } from './pages/tech/tech.component';
+import { PatentsComponent } from './pages/patents/patents.component';
 
 const routes: Routes = [
   { path: 'portfolio', component: ProjectsComponent, data: {title: 'David Nimon - Portfolio' } },
   { path: 'resume', component: ResumeComponent, data: {title: 'David Nimon - Resume' } },
   { path: 'technologies', component: TechComponent, data: {title: 'David Nimon - Technologies' } },
+  { path: 'patents', component: PatentsComponent, data: {title: 'David Nimon - Patents' } },
   { path: '**', component: HomeComponent, data: {title: 'David Nimon' } }
 ];
 
